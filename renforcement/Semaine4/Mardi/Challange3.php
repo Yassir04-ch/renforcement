99e53e6be83fae1daa68d7989011844c58f5157b
yassir cherqui <yassirch245@gmail.com>
2026-04-14 21:50:15 +0200

    ajouter challange3

--- renforcement/Semaine4/Mardi/Challange3.php
+++ renforcement/Semaine4/Mardi/Challange3.php
@@ -1,3 +1,38 @@
 <?php
 
 
+final class Coffre{
+    private string $cle;
+    private string $mode;
+
+    public function __construct(string $cle,string $mode)
+    {
+        $this->cle = $cle;
+        $this->mode = $mode;
+     }
+
+     final public function chiffrement($text , $num){
+        
+        for($i = 0 ; $i<strlen($text) ;$i++){
+           $result = chr(ord($text[$i] + $num));
+        }
+        return $result;
+     }
+
+     final public function masque($password){
+        $masq = substr($password ,0,2)."***".substr($password ,-2);
+        return $masq;
+     }
+
+
+}
+
+
+class Hack extends Coffre
+{
+
+}
+
+$c = new Hack("test","test");
+
+echo $c->masque("password");
